66435d303dbfad6dfbd296d877f846108227f2e8
João Moraski <joaomoraskilunkes@gmail.com>
2022-09-15 16:49:14 -0300

    v4 e v5 code

---
+++ primeiros-passos/helloworld/v5/helloworld.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+// constantes em alguns casos podem melhorar a performance do codigo
+// Este nao é um deles kkkkkk
+const prefixoOlaPortugues = "Olá, "
+
+// separação da regra de negocio
+func Ola(nome string) string {
+	if nome == "" {
+		nome = "mundo"
+	}
+	return prefixoOlaPortugues + nome
+}
+
+func main() {
+	fmt.Println(Ola("mundo"))
+}
---
+++ primeiros-passos/helloworld/v5/helloworld_test.go
@@ -0,0 +1,26 @@
+package main
+
+import "testing"
+
+func TestOla(t *testing.T) {
+
+	verificarSaidaTeste := func(t *testing.T, resultado, esperado string) {
+		t.Helper()
+		if resultado != esperado {
+			t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
+		}
+	}
+
+	// Apresentado os subtestes
+	t.Run("diz olá para as pessoas", func(t *testing.T) {
+		resultado := Ola("Moraski")
+		esperado := "Olá, Moraski"
+		verificarSaidaTeste(t, resultado, esperado)
+	})
+
+	t.Run("diz 'Olá mundo' quando uma string vazia for passada", func(t *testing.T) {
+		resultado := Ola("")
+		esperado := "Olá, mundo"
+		verificarSaidaTeste(t, resultado, esperado)
+	})
+}
