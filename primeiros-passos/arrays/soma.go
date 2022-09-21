9b6a7760ced233cd3807dccfcd4bf09b84943c79
Joao Moraski <joaomoraskilunkes@gmail.com>
2022-09-21 19:11:45 -0300

    adicionado parte de slices slices

--- primeiros-passos/arrays/soma.go
+++ primeiros-passos/arrays/soma.go
@@ -1,8 +1,8 @@
 package arrays
 
-func Soma(numeros [5]int) (resultado int) {
+func Soma(numeros []int) (resultado int) {
 	// usar o range para ajudar a limpar o codigo
-	// _ blank identifier para ignorar o valor do range
+	// _ blank identifier para ignorar o valor do indice
 	for _, numero := range numeros {
 		resultado += numero
 	}
--- primeiros-passos/arrays/soma_test.go
+++ primeiros-passos/arrays/soma_test.go
@@ -3,12 +3,27 @@ package arrays
 import "testing"
 
 func TestSoma(t *testing.T) {
-	numeros := [5]int{1, 2, 3, 4, 5}
 
-	resultado := Soma(numeros)
-	esperado := 15
+	t.Run("Array com 5 elementos", func(t *testing.T) {
+		numeros := []int{1, 2, 3, 4, 5}
 
-	if resultado != esperado {
-		t.Errorf("Esperado '%s', resultado '%s', dado %v", esperado, resultado, numeros)
-	}
+		resultado := Soma(numeros)
+		esperado := 15
+
+		if resultado != esperado {
+			t.Errorf("Esperado '%d', resultado '%d', dado %v", esperado, resultado, numeros)
+		}
+	})
+
+	// t.Run("Slice de qualquer tamanho", func(t *testing.T) {
+	// 	numeros := []int{1, 2, 3, 4, 5, 6}
+
+	// 	resultado := Soma(numeros)
+	// 	esperado := 21
+
+	// 	if resultado != esperado {
+	// 		t.Errorf("Esperado '%d', resultado '%d', dado %v", esperado, resultado, numeros)
+	// 	}
+	// })
+
 }
